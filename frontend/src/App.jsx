b1255ce0e8ead5dbbdbc05f9e1d6c44a2379ffae
tiwwongsakron <tiwwongsakron@gmail.com>
2026-04-19 00:07:45 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+const API_URL = "https://cw1idz62fc.onrender.com";
 function App() {
   // ==========================================
   // ส่วนที่ 1: การสร้าง State (ตัวแปรสำหรับเก็บข้อมูลของหน้าเว็บ)
